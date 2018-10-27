b551ba6a7918db769f2f1d39cd38642c3b24ebca
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2018-10-27 19:39:31 +0200

    Obrazy - Laby z Pythona

--- Przetwarzanie Obrazów/Lab01.py
+++ Przetwarzanie Obrazów/Lab01.py
@@ -1,9 +1,14 @@
 from math import sqrt
 
-#Zad 1 Trza zrobić
+#Zad 1
+def median(a, b, c):
+	x = [a, b, c]
+	x.remove((min(x)))
+	x.remove(max(x))
+	return x[0]
 
 #Zad 2
-def median(numbers):
+def median_sorting(numbers):
 	return sorted(numbers)[len(numbers)//2]
 
 #Zad 3
@@ -22,10 +27,24 @@ def sum_of_powers(numbers, k):
 
 #Zad 6
 def distance(v1, v2):
-	return sqrt(sum([(x - y)**2 for x, y in zip(v1, v2)]))
+	if len(v1) != len(v2):
+		return -1
+	else:
+		return sqrt(sum([(x - y)**2 for x, y in zip(v1, v2)]))
 
-#Zad 7 Trza zrobić
+#Zad 7
+def sum_start_end(numbers, x, r):
+	total = 0
+	for i in range(x-r, x+r+1):
+		if i < 0 or i > len(numbers)-1:
+			pass
+			#total += 3
+		else:
+			total += numbers[i]
 
-#Zad 8 Trza zrobić
+	return total
 
-
+#Zad 8
+def sum_of_matrix(x, y):
+	matrix = [[x[i]*y[j] for j in range(len(y))] for i in range(len(x))]
+	return sum([sum(row) for row in matrix])
