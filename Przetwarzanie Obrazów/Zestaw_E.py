dfd475da9df2239a9d37850308ea9af1a7d7a3e9
JedrzejKuczynski <jk.kuczynski@gmail.com>
2019-01-07 13:04:33 +0100

    Po - zestaw_E koniec

--- Przetwarzanie Obrazów/Zestaw_E.py
+++ Przetwarzanie Obrazów/Zestaw_E.py
@@ -10,11 +10,11 @@ test_kernel = np.array([[0, 1, 0],
 						[0, 1, 1],
 						[0, 0, 0]])
 
-print(test_image, "\n")
 
 def zadE1(image, kernel):
-	k = len(kernel) // 2
-	padded_image = np.pad(image, ((k, k), (k, k)), "constant")
+	k_col = kernel.shape[1] // 2
+	k_row = kernel.shape[0] // 2
+	padded_image = np.pad(image, ((k_row, k_row), (k_col, k_col)), "constant")
 	for y, x in np.ndindex(image.shape):
 		result = np.sum(padded_image[y:y+kernel.shape[0], x:x+kernel.shape[1]]*kernel)
 		if result > 0:
@@ -23,12 +23,19 @@ def zadE1(image, kernel):
 			image[y, x] = 0
 	return image
 
-#print(zadE1(test_image, test_kernel), "\n")
+def testE1():
+	result = zadE1(test_image.copy(), test_kernel)
+	expected = [[1, 1, 1, 1],
+				[1, 1, 1, 1],
+				[1, 1, 1, 1],
+				[1, 1, 1, 1]]
+	np.testing.assert_array_equal(result, expected)
 
 
 def zadE2(image, kernel):
-	k = len(kernel) // 2
-	padded_image = np.pad(image, ((k, k), (k, k)), "constant")
+	k_col = kernel.shape[1] // 2
+	k_row = kernel.shape[0] // 2
+	padded_image = np.pad(image, ((k_row, k_row), (k_col, k_col)), "constant")
 	for y, x in np.ndindex(image.shape):
 		result = np.sum(padded_image[y:y+kernel.shape[0], x:x+kernel.shape[1]]*kernel)
 		if result == np.sum(kernel):
@@ -38,4 +45,47 @@ def zadE2(image, kernel):
 	return image
 
 
-print(zadE2(test_image, test_kernel))
+def testE2():
+	result = zadE2(test_image.copy(), test_kernel)
+	expected = [[0, 0, 0, 0],
+				[0, 1, 1, 0],
+				[0, 0, 0, 0],
+				[0, 1, 0, 0]]
+	np.testing.assert_array_equal(result, expected)
+
+def zadE3(image, kernel):
+	kernel_first = kernel[::-1, ::-1]
+	image = zadE1(image, kernel_first)
+	image = zadE2(image, kernel)
+	return image
+
+
+def testE3():
+	result = zadE3(test_image.copy(), test_kernel)
+	expected = [[0, 0, 0, 0],
+ 				[1, 1, 1, 0],
+ 				[0, 1, 1, 0],
+ 				[0, 1, 1, 0]]
+	np.testing.assert_array_equal(result, expected)
+
+
+def zadE4(image, kernel):
+	kernel_first = kernel[::-1, ::-1]
+	image = zadE2(image, kernel_first)
+	image = zadE1(image, kernel)
+	return image
+
+
+def testE4():
+	result = zadE4(test_image.copy(), test_kernel)
+	expected = [[0, 1, 1, 1],
+				[1, 1, 1, 1],
+				[0, 1, 0, 1],
+				[0, 0, 0, 0]]
+	np.testing.assert_array_equal(result, expected)
+
+
+testE1()
+testE2()
+testE3()
+testE4()
