b97566abc4a6f927b5ea027636369a8899e324f8
Jedrzej <jk.kuczynski@gmail.com>
2019-04-23 15:57:51 +0200

    BS - Lab03, zad02. Chyba działa

--- Bioinformatyka_Strukturalna/Lab03/zad02.py
+++ Bioinformatyka_Strukturalna/Lab03/zad02.py
@@ -1,10 +1,10 @@
 import argparse
 import json
 
-"""parser = argparse.ArgumentParser(description="Zadanie 2 z Laboratorium #3 \
+parser = argparse.ArgumentParser(description="Zadanie 2 z Laboratorium #3 \
                                  z Bioinformatyki Strukturalnej")
 parser.add_argument("json", help="plik JSON")
-args = parser.parse_args()"""
+args = parser.parse_args()
 
 
 def zad02(pdb):
@@ -39,11 +39,43 @@ def zad02(pdb):
     # print(data_readable_pairs)
     # print(data_readable_nts)
 
-    for i in range(len(data_readable_pairs) - 1):
-        first_nt = data_readable_pairs[i]
-        second_nt = data_readable_pairs[i+1]
-        if first_nt[4] == "cWW" and second_nt[4] == "tSH":
-            print(first_nt, second_nt)
+    for key in data_readable_nts.keys():
+        chain_dict = data_readable_nts[key]
+        for i in range(len(chain_dict) - 5):
+            first_nt = chain_dict[i][0]
+            first_nt_letter = chain_dict[i][2]
+            first_nt_number = chain_dict[i][3]
+            last_nt = chain_dict[i+5][0]
+            last_nt_letter = chain_dict[i+5][2]
+            last_nt_number = chain_dict[i+5][3]
+            first_pair = [tup for tup in data_readable_pairs
+                          if tup[1] == first_nt
+                          and tup[2] == last_nt
+                          and tup[4] == "cWW"]
+            if first_pair:
+                second_nt = chain_dict[i+1][0]
+                second_nt_letter = chain_dict[i+1][2]
+                penultimate_nt = chain_dict[i+4][0]
+                penultimate_nt_letter = chain_dict[i+4][2]
+                second_pair = [tup for tup in data_readable_pairs
+                               if tup[1] == second_nt
+                               and tup[2] == penultimate_nt
+                               and tup[4] == "tSH"]
+                if second_pair:
+                    unpaired_nt1 = chain_dict[i+2][0]
+                    unpaired_nt1_letter = chain_dict[i+2][2]
+                    unpaired_nt2 = chain_dict[i+3][0]
+                    unpaired_nt2_letter = chain_dict[i+3][2]
+                    unpaired = [tup for tup in data_readable_pairs
+                                if tup[1] == unpaired_nt1
+                                and tup[2] == unpaired_nt2]
+                    if not unpaired:
+                        seq_list = [first_nt_letter, second_nt_letter,
+                                    unpaired_nt1_letter, unpaired_nt2_letter,
+                                    penultimate_nt_letter, last_nt_letter]
+                        seq_string = "".join(seq_list)
+
+                        print(first_nt_number, seq_string, last_nt_number)
 
 
 zad02("1RLG.json")
