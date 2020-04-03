f68af0b892f36b58de9ef7248a570f33ea2a6796
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2020-04-03 14:47:52 +0200

    OWS - Cannon. Działa. Pamiętać o liczbie procesów

---
+++ OWS/Zadanie01/freivald.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.loadtxt("liczby.txt")
+b = np.loadtxt("liczby.txt")
+c = np.loadtxt("wynik.txt")
+
+c_flat = c.flatten()
+print(np.max(c_flat))
+
+output = []
+
+for _ in range(100_000):
+    r = np.random.randint(2, size=[a.shape[0]])
+    br = np.dot(b, r)
+    cr = np.dot(c, r)
+    abr = np.dot(a, br)
+    p = abr - cr
+    if not any(p):
+        output.append(True)
+
+if all(output):
+    print("Mnozenie macierzy poprawne!")
+else:
+    print("Mnozenei macierzy niepoprawne!")
--- OWS/Zadanie01/gen_danych.c
+++ OWS/Zadanie01/gen_danych.c
@@ -6,16 +6,18 @@ int main(int argc, char **argv) {
    FILE* plik;
    int i, j;
 
+   int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
    plik = fopen("liczby.txt", "w");
 
    if(plik == NULL) {
       printf("Blad otwarcia pliku \"liczby.txt\"\n");
       exit(0);
    }
- 
-   for(i = 0; i < 4; i++) {
-      for(j = 0; j < 4; j++)
-         fprintf(plik, "%6.1f", (float)i+j);
+
+   for(i = 0; i < 910; i++) {
+      for(j = 0; j < 910; j++)
+         fprintf(plik, "%f ", (float)numbers[i % 10]);
 
       fprintf(plik, "\n");
    }
