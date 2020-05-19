440ac4e3db7586059db3e7e6adac048ccf4cac06
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2020-05-19 20:25:17 +0200

    OWS - Zadanie 3. Wersje sekwencyjne

---
+++ OWS/Zadanie03/dzielenie.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <omp.h>
+
+
+int main(int argc, char* argv[]) {
+
+    int m = 0;
+    int n = 0;
+    int show_result = 0;
+    int range = 0;
+    bool *prime_numbers;
+
+    // Wczytanie wartości
+
+    if(argc != 4) {
+        printf("Nie podales argumentow! Przykladowe uruchomienie programu: ./dzielenie 10 49 1.\n M - poczatek zakresu, N - koniec zakresu. 0/1 - czy pokazac wynik\n");
+        return 0;
+    } else {
+        m = atoi(argv[1]);
+        n = atoi(argv[2]);
+        show_result = atoi(argv[3]);
+        range = n - m + 1;
+
+        if(m > n) {
+            printf("Przedzial <%d, %d> nie istnieje! Moze pomyliles kolejnosc liczb?\n", m, n);
+            return 0;
+        }
+
+        prime_numbers = (bool*)malloc(range*sizeof(prime_numbers));
+    }
+
+    for(int i = m; i <= n; i++) {
+        int root = (int)sqrt(i);
+
+        if(root < 2)
+            root = 2;
+
+        // printf("Dla liczby %d pierwiastek wynosi %d\n", i, root);
+
+        for(int j = 2; j <= root; j++) {
+            if(i % j == 0) {
+                // printf("Dziele %d przez %d\n", i, j);
+                prime_numbers[i - m] = true;
+                break;
+            }
+        }
+    }
+
+    if(show_result == 1) {
+
+        int counter = 0;
+
+        printf("\n");
+
+        for(int i = 0; i < range; i++)
+            if(!prime_numbers[i]) {
+                printf("%d ", i + 10);
+                counter++;
+
+                if(counter % 10 == 0)
+                    printf("\n");
+            }
+        printf("\n\n");
+        printf("W zakresie od %d do %d znaleziono %d liczb pierwszych.\n\n", m, n, counter);
+    }
+}
---
+++ OWS/Zadanie03/sito.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <omp.h>
+
+
+int main(int argc, char* argv[]) {
+
+    int m = 0;
+    int n = 0;
+    int n_root = 0;
+    int range = 0;
+    int show_result = 0;
+    bool *prime_check;
+    bool *primes_to_root;
+    int *primes_in_range;
+
+    // Wczytanie wartości
+
+    if(argc != 4) {
+        printf("Nie podales argumentow! Przykladowe uruchomienie programu: ./sito 10 49 1.\n M - poczatek zakresu, N - koniec zakresu. 0/1 - czy pokazac wynik\n");
+        return 0;
+    } else {
+        m = atoi(argv[1]);
+        n = atoi(argv[2]);
+        show_result = atoi(argv[3]);
+        n_root = int(sqrt(n));
+        range = n - m + 1;
+
+        if(m > n) {
+            printf("Przedzial <%d, %d> nie istnieje! Moze pomyliles kolejnosc liczb?\n", m, n);
+            return 0;
+        }
+
+        primes_to_root = (bool*)malloc((n_root + 2)*sizeof(primes_to_root));
+        primes_in_range = (int*)malloc(range*sizeof(primes_in_range));
+
+    }
+
+    // Sito <2, sqrt(n)>
+
+    prime_check = (bool*)malloc((n_root + 2)*sizeof(prime_check));
+
+    for(int i = 2; i < n_root + 2; i++)
+        if(prime_check[i] == false) {
+            primes_to_root[i] = true;
+
+            for(int j = i; j < n_root + 2; j += i)
+                prime_check[j] = true;
+        }
+
+        /*
+        for(int i = 0; i < n_root + 2; i++)
+            printf("%d ", prime_check[i]);
+
+        printf("\n\n");
+
+        for(int i = 0; i < n_root + 2; i++)
+            printf("%d\n", primes_to_root[i]);
+        */
+
+    // Sito <M, N>
+
+    for(int i = 0; i < n_root + 2; i++)
+        if(primes_to_root[i] == true) {
+            int lowest = floor(m / i) * i;
+
+            if(lowest < m)
+                lowest += i;
+
+            if(lowest == i)
+                lowest += i;
+
+            for(int j = lowest; j <= n; j += i)
+                primes_in_range[j - m] = true;
+        }
+
+        if(show_result == 1) {
+
+            int counter = 0;
+
+            printf("\n");
+
+            for(int i = m; i <= n; i++)
+                if(!primes_in_range[i - m]) {
+                    printf("%d ", i);
+                    counter++;
+
+                    if(counter % 10 == 0)
+                        printf("\n");
+                }
+            printf("\n\n");
+            printf("W zakresie od %d do %d znaleziono %d liczb pierwszych.\n\n", m, n, counter);
+        }
+}
