af0ee861b36fe39052294860d1ee5a2d75032345
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2020-08-31 18:35:39 +0200

    OWS - sprawko #1. Koniec

--- OWS/Zadanie01/cw8.c
+++ OWS/Zadanie01/cw8.c
@@ -4,6 +4,8 @@
 #include <limits.h>
 
 
+// Funkcja majaca na celu znalezienie procesu, ktory przechowuje wynik
+
 int find_index_of_minimum(int test_array[], int size){
     int min_index = -1;
     int min = INT_MAX;
@@ -33,19 +35,38 @@ int main(int argc, char* argv[]) {
 
     min_index = find_index_of_minimum(test_array, sizeof(test_array) / sizeof(int));
 
+    // Tworzenie "struktury" kraty NxN procesow, gdzie N to rozmiar tablicy
+
     i = rank / (sizeof(test_array) / sizeof(int));
     j = rank % (sizeof(test_array) / sizeof(int));
+
+    // Kazdy wiersz "i" procesow zajmuje sie jedna liczba w tablicy (znajdujacej sie pod indeksem "i")
+    // i zlicza, ile liczb jest od niej mniejszych (chodzac po kolumnach "j") poprzez okreslenie wartosci zmiennej "w",
+    // ktora dalej bedzie sumowana w ramach wiersza "i" procesow
+    // Drugi warunek zapewnia oryginalna kolejnosc liczb sobie rownych
 
     if((test_array[i] > test_array[j]) || (test_array[i] == test_array[j] && (i > j)))
         w = 1;
     else
         w = 0;
 
-    MPI_Comm_split(MPI_COMM_WORLD, i, j, &row_com);
+    MPI_Comm_split(MPI_COMM_WORLD, i, j, &row_com);  // Podzial komunikatora na N komunikatorow w ramach wiersza.
+    												 // Procesy o tym samym "i" naleza do tego samego komunikatora
+    												 // Nowa ranga procesu okreslana kolumna "j"
 
-    MPI_Reduce(&w, &index, 1, MPI_INT, MPI_SUM, 0, row_com);
+    MPI_Reduce(&w, &index, 1, MPI_INT, MPI_SUM, 0, row_com);  // Nastepuje wyzej wspomniane sumowanie.
+    														  // Wynik przechowywany jest w pierwszej kolumnie wierszy (procesie o randze 0)
 
-    MPI_Comm_split(MPI_COMM_WORLD, j, index, &col_com);
+    MPI_Comm_split(MPI_COMM_WORLD, j, index, &col_com);  // Kolejny podzial, tym razem w ramach kolumn.
+    													 // Nowa ranga procesu jest zliczona suma, co pozwoli na
+    													 // okreslenie pozycji liczby w posortowanej tablicy
+
+    // Ponizej nastepuje zebranie wynikow w procesie o randze 0 z poszczegolnych procesow w kolumnie 0.
+    // Jako ze kazdy z procesow w komunikatorze "col_com", w ramach jednej wartosci "j", znajduje sie w innym wierszu "i"
+    // a jego ranga w ramach komunikatora zalezy od obliczonej sumy
+    // To proces 0 w ramach komunikatora bedzie odbieral
+    // liczby znajdujace sie pod indeksem "i" w oryginalnej tablicy
+    // w kolejnosci posortowanej
 
     if(j == 0) {
         MPI_Gather(&test_array[i], 1, MPI_INT, result, 1, MPI_INT, 0, col_com);
